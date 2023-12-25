b9e51ad61b5d87060c42835b92ee674d90a3d47d
AyushTmg <ayushtmg20040722@gmail.com>
2023-12-25 17:27:13 +0545

    Init

---
+++ authentication/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display=['first_name','last_name','email','username', "is_staff", "is_active","is_superuser","last_login","date_joined"]
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "first_name","last_name","username","email", "password1", "password2", "is_staff",
+                "is_active","is_superuser", "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
---
+++ authentication/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    first_name=models.CharField(max_length=150)
+    last_name=models.CharField(max_length=150)
+    email=models.EmailField(unique=True)
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['first_name','last_name','username']
