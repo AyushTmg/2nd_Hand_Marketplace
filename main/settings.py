402a20d4d6c283e9f28ee8a852898c160e296901
AyushTmg <ayushtmg20040722@gmail.com>
2023-12-26 10:49:47 +0545

    Init: New app for marketplace  place initiaized

--- main/settings.py
+++ main/settings.py
@@ -12,17 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=uf3%)4vs%nga#@l!$#x050xeyon4ghq)4+%1wlu&4b_x)&6rj'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -38,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
+    'marketplace',
 ]
 
 MIDDLEWARE = [
