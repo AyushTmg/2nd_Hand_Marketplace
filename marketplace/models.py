470fa3df2ce1f00c0ec4ed5ea038f5bd72a28183
AyushTmg <ayushtmg20040722@gmail.com>
2023-12-27 22:33:09 +0545

    Models.py Updated

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class  Category(models.Model):
     titile=models.CharField(max_length=150,unique=True)
@@ -7,23 +8,32 @@ class  Category(models.Model):
         return self.titile
     
 class Item(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='item')
     title = models.CharField(max_length=200)
     description = models.TextField()
+    location=models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    price=models.PositiveIntegerField()
+    is_available=models.BooleanField(default=True)
     condition_choices={
         ('new', 'New'),
         ('like_new', 'Like New'),
         ('good', 'Good'),
         ('decent', 'Decent'),
     }
-    location=models.CharField(max_length=100)
+    negotiation_choices={
+        ('yes','Yes'),
+        ('no','No')
+    }
     condition=models.CharField(max_length=20, choices=condition_choices)
-    created_at = models.DateTimeField(auto_now_add=True)
+    is_negotiable=models.CharField(max_length=20, choices=negotiation_choices)
+
 
     def __str__(self):
         return self.title
     
 class Image(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE,related_name='image')
     image = models.ImageField(upload_to='item_images/')
 
     def __str__(self):
