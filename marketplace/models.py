0143338d7aa8a5cf09c1f73635df501b5a698f3e
AyushTmg <ayushtmg20040722@gmail.com>
2024-01-01 14:05:03 +0545

    Model message defined for chatting

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.conf import settings
+from django.conf import settings 
 
 class  Category(models.Model):
     titile=models.CharField(max_length=150,unique=True)
@@ -55,6 +55,17 @@ class Reply(models.Model):
     
     def __str__(self) -> str:
         return self.description
+    
+
+class Message(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender.username} to {self.receiver.username}: {self.content}'
+
     
 
     
