987381180c28078bb4b35a04224fc9832eb0af2c
AyushTmg <ayushtmg20040722@gmail.com>
2024-01-16 20:16:18 +0545

    Refactor:Removed the previously used method of adding static css and js

--- static/js/marketplace/item_detail.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('show-comments').addEventListener('click', function () {
-        var commentsContainer = document.getElementById('comments-container');
-        commentsContainer.style.display = (commentsContainer.style.display === 'none') ? 'block' : 'none';
-    });
-});
